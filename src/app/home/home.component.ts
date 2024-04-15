be04f2fd74a91605b39e7cda6eefe1ffaa9d2979
capmec <capmec@gmail.com>
2024-04-15 22:06:32 +0200

    Add an interpolation to a component’s template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,18 +10,17 @@ import { HousingLocation } from '../housinglocation';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by City" />
+        <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
         [housingLocation]="housingLocation"
-        ]
       ></app-housing-location>
     </section>
   `,
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
